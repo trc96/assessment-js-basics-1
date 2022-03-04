5ba4f4996b75bcaa91583abd7bda382f1458344b
trc96 <toddcrandall1996@gmail.com>
2022-03-04 08:18:35 -0700

    sldc.js file completed.

--- sldc.js
+++ sldc.js
@@ -5,12 +5,12 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
@@ -18,4 +18,8 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.";
+const design = "This second step focuses on creating software architecture, prototype, and user experience design.";
+const implementation = "In this third step, code is finally written, sometimes by the thousands, to create what was planned and designed. The project as a whole could be shipped, or parts that are intended to go out in smaller updates.";
+const testingAndIntegration = "In this fourth step, either the developers or a team of testers focus on testing the written code for bugs and other inconsistencies. This can be done manuall or automatically. Follwing this statement would be deployment, assuming testers and developers could ensure a foolproof delivery of the software.";
+const maintain = "In this final step, developers are intending to provide corrective or perfective maintenance. As new technologies and changing user requirments come into play, the software team needs to iterate through the entire software development life cycle to work on the new change.";
