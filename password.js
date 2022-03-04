29f7d6ee8ca25f570753b1133f8a0337ebdef7fb
trc96 <toddcrandall1996@gmail.com>
2022-03-04 10:04:28 -0700

    Implemented a spacing conditional to the user input

--- password.js
+++ password.js
@@ -5,14 +5,14 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
-console.log('Welcome to the password validator tool! Reminder: your password must be at least 10 characters long.');
+console.log('Welcome to the password validator tool! Reminder: your password must be at least 10 characters long and contain no spaces.');
 
 reader.question("Please enter your password: ", function(input){
 
-    if (input.length >= 10) {
+    if (input.length >= 10 && !input.includes(" ")) {
         console.log('Password Success.');
     } else {
-        console.log('Password Failure, needs to be at least 10 characters long.');
+        console.log('Password Failure, needs to be at least 10 characters long and contain no spaces.');
     }
 
 	reader.close()
